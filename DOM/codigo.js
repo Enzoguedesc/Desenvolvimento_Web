5a7da3591c008c6d3e7d653e41a3731a06307a8b
Enzo Guedes Cardoso <161377810+Enzoguedesc@users.noreply.github.com>
2024-11-04 17:00:30 -0300

    Update codigo.js

--- DOM/codigo.js
+++ DOM/codigo.js
@@ -1,15 +1,77 @@
 const container = document.getElementById('container');
 
-const montaCartao = (atleta) => `
-<article>
-    <h1>${atleta.nome}</h1>
-    <img src=${atleta.imagem}>
-    <p>${atleta.detalhes}</p>
-</article>
-`;
+const pega_json = async (caminho) => {
+    const resposta = await fetch(caminho);
+    const dados = await resposta.json();
+    return dados;
+}
 
-let conteudo = ''
+const trataClick = ( e ) => {
+    const id = e.currentTarget.dataset.id;
+    const artigo = e.currentTarget;
 
-dados.forEach( (elemento) => conteudo += montaCartao(elemento));
+    //cookie
+    document.cookie = `id=${id}`
+    document.cookie = `nome=${artigo.dataset.nome}`
+    document.cookie = `imagem=${artigo.dataset.caminhoImagem}`
 
-container.innerHTML = conteudo;
+
+    //session
+    sessionStorage.setItem('id', id);
+    sessionStorage.setItem('nome', artigo.dataset.nome);
+    sessionStorage.setItem('atleta', JSON.stringify(artigo.dataset));
+
+    //local
+    localStorage.setItem('id', id);
+
+
+    window.location = `outra.html?id=${id}`;
+}
+
+const montaCartao = (atleta) => { 
+    const cartao = document.createElement('article');
+    const nome = document.createElement('h1');
+    const imagem = document.createElement('img');
+    const descri = document.createElement('p');
+
+    /*const link = document.createElement('a');*/
+
+    nome.innerHTML = atleta.nome;
+    cartao.appendChild(nome);
+
+    imagem.src = atleta.imagem;
+    cartao.appendChild(imagem);
+
+    descri.innerHTML = atleta.detalhes;
+    cartao.appendChild(descri);
+
+
+    /*link.innerHTML = "Saiba mais"
+    link.href = `outra.html?id=${atleta.id}&altura=${atleta.altura};`
+    cartao.appendChild(link);*/
+
+    cartao.onclick = trataClick;
+
+    cartao.dataset.id = atleta.id;
+    cartao.dataset.nome = atleta.nome;
+    cartao.dataset.caminhoImagem = atleta.imagem;
+
+
+
+
+    container.appendChild(cartao);
+}
+
+pega_json("https://botafogo-atletas.mange.li/2024-1/feminino").then(
+    ( obj ) => {
+        console.log('isso imprime depois');
+        obj.forEach( (elemento) => montaCartao(elemento));
+    }
+)
+
+console.log('isso imprime antes');
+
+
+
+
+
